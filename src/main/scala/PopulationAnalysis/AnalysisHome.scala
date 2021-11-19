835b2ae53012e242cf50edd16d0db9a78bd43bbd
cselling <42050244+cselling@users.noreply.github.com>
2021-11-19 13:49:03 -0800

    Added query
    Added first query in analysis for world wide populations by year

--- src/main/scala/PopulationAnalysis/AnalysisHome.scala
+++ src/main/scala/PopulationAnalysis/AnalysisHome.scala
@@ -1,15 +1,34 @@
 package PopulationAnalysis
 
 import scala.io.StdIn.readInt
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
 
 object AnalysisHome extends App {
+  val DATA_FILE = "Data/WPP2019_PopulationBySingleAgeSex_1950-2019.csv"
   def dataAnalysis(): Unit =
   {
+    val spark = SparkSession
+      .builder()
+      .appName("Hello Hive")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    val sc = spark.sparkContext
+    spark.sparkContext.setLogLevel("ERROR")
     var menuSelection = 0;
     do{
       menuSelection = AnalysisMenu()
       menuSelection match {
-        case 1 =>{} // Please add query
+        case 1 =>{
+          val df = spark.read.option("header","true").option("inferSchema", "true").csv(DATA_FILE)
+          df.persist(StorageLevel.MEMORY_ONLY)
+          df.where(df("Time") === "2019").groupBy("LocID", "Location", "Time").avg("PopMale", "PopFemale").sort("Time").show(false)
+          df.groupBy("Time").sum("PopMale", "PopFemale").sort(df("Time").desc).show(false) // shows worldwide populations
+        } // Please add query
         case 2 => {}// Please add Query
         case 3 => {}// Please add query
         case 4 => {}// Please add query
